e3948977c3a74ec1029fadebdb35a0385d67528f
hay-0le <haley.olsen1@gmail.com>
2020-02-01 21:34:58 -0800

    Switched from mongoDB to postgres due to changed structure of the data

---
+++ pg-db/schemas.js
@@ -0,0 +1,56 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv').config();
+
+const connectionString = `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
+
+const pool = new Pool({
+  connectionString: connectionString
+})
+
+pool.on('connect', () => {
+
+  const createTablesQuery = `
+    CREATE SCHEMA IF NOT EXISTS auxium AUTHORIZATION postgres;
+
+    CREATE TABLE IF NOT EXISTS auxium.users (
+      userid integer NOT NULL,
+      playlists integer [],
+      CONSTRAINT users_pk PRIMARY KEY (userid)
+      );
+
+    ALTER TABLE auxium.users OWNER TO postgres;
+
+    CREATE TABLE IF NOT EXISTS auxium.playlists (
+      playlistid integer NOT NULL,
+      playlistname character varying(25)
+      songs integer [],
+      CONSTRAINT playlist_pk PRIMARY KEY (playlistid)
+    );
+
+    ALTER TABLE auxium.playlists OWNER TO postgres;
+
+    CREATE TABLE IF NOT EXISTS auxium.song (
+      songid integer NOT NULL,
+      url character varying(200),
+      title character varying(50),
+      artist character varying(50),
+      album character varying(50),
+      year integer,
+      duration integer,
+      CONSTRAINT playlist_pk PRIMARY KEY (playlistid)
+    )
+
+    ALTER TABLE auxium.song OWNER TO postgres;
+
+  `;
+
+  pool.query(createTablesQuery)
+    .then((res) => {
+      console.log("Success creating tables: ", res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log("ERROR creating tables: ", err);
+      pool.end();
+    })
+})
