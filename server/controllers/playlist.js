78cd925fe4b110b87dee794ca82ca492259fab5d
hay-0le <haley.olsen1@gmail.com>
2020-02-05 09:35:50 -0800

    Added functionality to addSong, to set information to eventually be passed into db, into variables

--- server/controllers/playlist.js
+++ server/controllers/playlist.js
@@ -1,16 +1,27 @@
-const { Song, db } = require('../../db/index.js');
 
-//update
+//update playlist with new song
 const addSong = (req, res) => {
   //save song to playlist
-  let song = new Song(req.body);
-  song.save((err, results) => {
-    if (err) {
-      console.log('Error adding song... ', err)
-    } else {
-      console.log('Song added to playlist')
-    }
-  })
+  let playlist = req.body.params.playlist;
+  let newSong = req.body.params.song;
+  // console.log("PLYLIST", playlist)
+  // console.log("SONG", newSong)
+  //make an array of artists
+
+  let id = newSong.id;
+  let url = newSong.external_urls.spotify;
+  let title = newSong.name;
+  let artists = newSong.artists.map(artist => artist.name);
+  console.log(artists)
+  let href = newSong.href;
+  let year = newSong.album.release_date.slice(0, 4);
+  let album = newSong.album.name;
+  let duration = newSong.duration_ms;
+
+  console.log(`OBJECT TO SAVE:   id=${id}   url=${url}  title=${title}  artists=${artists}  href=${href}  year=${year}  album=${album}  duration=${duration}`)
+  //to save: href, exteernal_urls.spotify,id, name, type
+
+  //add to database
 }
 
 //create
