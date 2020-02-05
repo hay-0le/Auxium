c2ae553f0d1ce0f309a85023fc98fa1542633afe
hay-0le <haley.olsen1@gmail.com>
2020-02-04 20:36:51 -0800

    Removed commented out logic for api routes Added comments Rearranged imports

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
-const request = require('request');
+///// IMPORTS /////
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
 const querystring = require('querystring');
+const request = require('request');
+const cors = require('cors');
 const path = require('path');
 
+///// IMPORT CONTROLLERS
+const authentication = require('./controllers/authentication.js');
+const mainPlayer = require('./controllers/mainPlayer.js');
+const playlist = require('./controllers/playlist.js');
+
+///// ENVIRONMENTAL VARIABLES /////
 require('dotenv').config();
 const port = process.env.PORT;
-const redirect_uri = process.env.REDIRECT_URI || 'http://localhost:' + port + '/callback';
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
 
-const authentication = require('./controllers/authentication.js')
+const redirect_uri = 'http://localhost:' + port + '/callback';
 
+///// EXPRESS SERVER /////
 const express = require('express');
 const app = express();
 
@@ -19,7 +28,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
-// Enabling CORS
+///// ENABLING CORS /////
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
@@ -31,16 +40,6 @@ app.use((req, res, next) => {
   next();
 });
 
-let buildRandomString = (length) => {
-  let str = '';
-  let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-  while (str.length < length) {
-    str += letters[Math.floor(Math.random() * letters.length)];
-  }
-  return str;
-}
-
 
 app.get('/', (req, res) => {
   //home page
@@ -48,111 +47,26 @@ app.get('/', (req, res) => {
 })
 
 
+///// AUTHENTICATION ROUTES /////
 app.get('/login', authentication.login);
-//   let state = buildRandomString(16);
-//   let scope = 'user-read-email user-read-private user-read-playback-state streaming';
-//   res.cookie('spotify_auth_state', state);
-
-//   res.redirect('https://accounts.spotify.com/authorize?' +
-//     querystring.stringify({
-//       response_type: 'code',
-//       client_id: process.env.CLIENT_ID,
-//       scope: scope,
-//       redirect_uri: redirect_uri,
-//       state: state
-//     }))
-// })
 
 //route after authorization server returns authorization code
 //Send Client ID and Secret code in headers along with authorization code
 //Client then sends authorization code back, to exchange for access token
 app.get('/callback', authentication.getAccessToken);
-  // let code = req.query.code || null;
-  // let state = req.query.state || null;
-  // let storedState = req.cookies ? req.cookies['spotify_auth_state'] : null;
 
-  // if (state === null || state !== storedState) {
-  //   res.redirect('/#' + querystring.stringify({
-  //     error: 'state_mismatch'
-  //   }));
-  // } else {
-  //   res.clearCookie('spotify_auth_state');
-
-  //   let authOptions = {
-  //     url: 'https://accounts.spotify.com/api/token',
-  //     form: {
-  //       code: code,
-  //       redirect_uri: redirect_uri,
-  //       grant_type: 'authorization_code'
-  //     },
-  //     headers: {
-
-  //       'Authorization': 'Basic ' + (new Buffer.from(
-  //         process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET
-  //         ).toString('base64'))
-  //     },
-  //     json: true
-  //   }
-
-  //   request.post(authOptions, (err, response, body) => {
-  //     if (response.statusCode !== 200 || err) {
-  //       console.log('error: ', err);
-  //       res.redirect('/#' +
-  //       querystring.stringify({
-  //           error: 'invalid_token'
-  //         })
-  //         );
-
-  //     } else {
-  //       let access_token = body.access_token;
-  //       let refresh_token = body.refresh_token;
-
-  //       let options = {
-  //         url: 'https://api/spotify.com/v1/me',
-  //         headers: { 'Authorization': 'Bearer' + access_token },
-  //         json: true
-  //       }
-
-  //       //use access token to access the Spotify APi
-  //       request.get(options, (err, response, body) => {
-  //         console.log("body", body);
-  //         console.log("response", response)
-  //       })
-
-  //       res.redirect('http://localhost:3001/#' +
-  //         querystring.stringify({
-  //           access_token: access_token,
-  //           refresh_token: refresh_token
-  //         }));
-  //       }
-  //   })
-  // }
-// })
+//Will need to request new access token after one hour --> use refresh token to do so
+app.get('/refresh_token', authentication.getRefreshToken);
 
 
-//request access using refresh token
-app.get('/refresh_token', authentication.getRefreshToken)
-;//username=6auyw95oqfdnzc95jxyk5waqd'
+
+///// DATABASE ROUTES /////
+// app.get('/playlist/:playList', controllers.getPlaylist);
+
+// app.get('/playlist/:addSong', controllers.addSong);
 
 
-app.get('/playlist/:playList', controllers.getPlaylist);
-
-app.get('/playlist/:addSong', controllers.addSong);
 
 app.listen(port, () => {
   console.log('Listening on port: ', port)
 })
-
-
-
-
-      // var options = {
-      //   url: 'https://api/spotify.com/v1/me',
-      //   headers: { 'Authorization': 'Bearer' + access_token},
-      //   json: true
-      // }
-
-      // //use access token to access the Spotify APi
-      // request.get(options, (err, response, body) => {
-      //   console.log(body)
-      // })
