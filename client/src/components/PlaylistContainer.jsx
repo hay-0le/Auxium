87824e00a0f329e6d9c0471f7f9eff7e201cdab6
hay-0le <haley.olsen1@gmail.com>
2020-02-06 09:05:10 -0800

    Added form to create a new playlist

--- client/src/components/PlaylistContainer.jsx
+++ client/src/components/PlaylistContainer.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import SongsList from './SongsList.jsx';
 import Playlists from './Playlists.jsx';
 
-const PlaylistContainer = ({ songs, playlist, playlists, changeSong}) => {
+const PlaylistContainer = ({ songs, playlist, playlists, changeSong, addPlaylist}) => {
 
 
   return (
     <div id="playlistcontainer">
-      <h3>Player: </h3>
-    <SongsList changeSong={changeSong} playlist={playlist} songs={songs}/>
+      <h3>Playlist: </h3>
+      <div id="create-playlist">
+        <form className="form" id="add-playlist-form">
+          <input
+            type="text"
+            className="input"
+            id="add-playlist"
+            placeholder="Playlist Name"
+          />
+          <button className="button is-info" onClick={addPlaylist}>
+            Create New Playlist
+          </button>
+        </form>
+      </div>
     <Playlists playlists={playlists}/>
+    <SongsList changeSong={changeSong} playlist={playlist} songs={songs}/>
     </div>
   )
 }
