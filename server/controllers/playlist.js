a1daaf52df3aa5a560a047b303c95d56222e429f
hay-0le <haley.olsen1@gmail.com>
2020-02-06 09:03:40 -0800

    Added function to create playlist in pg-db from api route: create_playlist

--- server/controllers/playlist.js
+++ server/controllers/playlist.js
@@ -13,25 +13,21 @@ const addSong = (req, res) => {
 
   let playlist = req.body.params.playlist;
   let newSong = req.body.params.song;
-  // console.log("PLYLIST", playlist)
-  // console.log("SONG", newSong)
-  //make an array of artists
 
   let id = newSong.id;
   let url = newSong.external_urls.spotify;
   let href = newSong.href;
   let title = newSong.name;
+  //map array of artist objects, to return array of artists names only
   let artists = newSong.artists.map(artist => artist.name);
   let year = newSong.album.release_date.slice(0, 4);
   let album = newSong.album.name;
   let duration = newSong.duration_ms;
 
-  console.log(`OBJECT TO SAVE:   id=${id}   url=${url}  title=${title}  artists=${artists}  href=${href}  year=${year}  album=${album}  duration=${duration}`)
+  pool.connect()
+    .then(() => {
 
-  //add to database
-  pool.on('connect', () => {
-
-    let queryString = `INSERT INTO songs (songid, url, href, title, artists, album, year, duration, playlist) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+    let queryString = `INSERT INTO auxium.songs (songid, url, href, title, artists, album, year, duration, playlist) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
 
     pool.query(queryString, [id, url, href, title, artists, album, year, duration, playlist])
       .then(response => {
@@ -39,64 +35,101 @@ const addSong = (req, res) => {
         pool.end();
       })
       .catch(err => {
-        console.log(`Error adding ${title} to playlist: `, err)
+        console.log(`ERROR adding ${title} to playlist: `, err);
+        pool.end();
       })
+  })
+
+  .catch(err => {
+    console.log("ERROR connecting to pool to add song: ", err);
   })
 }
 
-//create
-const createPlaylist = (req, res) => {
 
-
-}
-
-//delete song from database
+//delete song from playlist
 const deleteSong = (req, res) => {
   //TO DO: Should reference song by name, or url, or id?
 
   let song = req.body.params;
   console.log("song: ", song);
 
-  pool.on('connect', () => {
+  pool.connect()
+    .then(() => {
 
-    let queryString = `DELETE FROM songs WHERE title = ${song}`;
+    let queryString = `DELETE FROM auxium.songs WHERE title = ${song}`;
 
     pool.query(queryString)
       .then(response => {
         console.log(`Successfully deleted song ${song}`)
       })
       .catch(err => {
-        console.log(`Error deleteing song ${song}: `, error)
+        console.log(`ERROR deleteing song ${song}: `, error)
       })
-  })
+    })
+
+    .catch(err => {
+      console.log("ERROR connecting to database to delete song: ", err)
+    })
 
 
 
 }
 
-//Read
+//Return requested playlist
 const getPlaylist = (req, res) => {
-  let playlist = req.body.params.playList;
+  let playlist = req.body.params.playlist;
   console.log('playlist:', playlist)
 
-  pool.on('connect', () => {
-    let queryString = `SELECT * FROM playlists WHERE playlistname = ${playlist}`;
+  pool.connect()
+    .then(() => {
+      let queryString = `SELECT * FROM auxium.playlists WHERE playlistname = ${playlist}`;
 
-    pool.query(queryString)
-    .then(data => {
-      console.log(`Successfully retreived playlist: ${playlist}`);
-      res.send(data);
+      pool.query(queryString)
+      .then(data => {
+        console.log(`Successfully retreived playlist: ${playlist}`);
+        res.send(data);
+      })
+      .catch(err => {
+        console.log(`ERROR retreiving playlist ${playList}: `, err)
+      })
+
     })
     .catch(err => {
-      console.log(`Error retreiving playlist ${playList}: `, err)
+      console.log("ERROR connecting to database while getting playlist:", err)
     })
-  })
 
 }
 
+//create new (empty) playlist
+const addPlaylist = (req, res) => {
+  let newPlaylist = req.body.params.newPlaylist;
+  let newPlaylistId = req.body.params.newPlaylistId;
+  console.log('NEW playlist:', newPlaylist, newPlaylistId)
+
+  pool.connect()
+    .then(() => {
+      let queryString = `INSERT INTO auxium.playlists (playlistid, playlistname) VALUES ($1, $2)`;
+
+      pool.query(queryString, [newPlaylistId, newPlaylist])
+      .then(data => {
+        console.log(`Successfully created playlist: ${newPlaylist}`);
+        res.send(data);
+      })
+      .catch(err => {
+        console.log(`ERROR creating playlist ${newPlaylist}: `, err)
+      })
+
+    })
+    .catch(err => {
+      console.log("ERROR connecting to database while creating playlist:", err)
+    })
+
+}
+
+
 module.exports = {
-  getPlaylist,
   addSong,
-  createPlaylist,
-  deleteSong
+  deleteSong,
+  getPlaylist,
+  addPlaylist
 }
