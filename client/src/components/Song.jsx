eec3d8a2f7aa464cca51db2c7ea98c06bc83a448
hay-0le <haley.olsen1@gmail.com>
2020-02-17 19:10:15 -0800

    Restructured props and data in Song component

--- client/src/components/Song.jsx
+++ client/src/components/Song.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Song = ({ song, id, changeSong}) => {
 
-  let title = song.song;
-  let artist = song.artist;
-  let length = song.length;
+  let title = song.title;
+  // let artists = song.artists.map(artist => artist.name);
+  let artists = song.artists;
+  let length = String(song.duration / 60000).slice(0, 3);
   let coverArt = song.coverArt;
 
   return (
     <div id={id} onClick={changeSong}>
-      {`${id}: ${title} by ${artist} - (${length} minutes)`}
+      {`${id + 1}: ${title} by ${artists} - (${length} minutes)`}
     </div>
   )
 }
