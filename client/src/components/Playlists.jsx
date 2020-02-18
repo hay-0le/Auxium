075dab7e69f0563237cffeda6d5a7995e4301ef6
hay-0le <haley.olsen1@gmail.com>
2020-02-17 19:07:27 -0800

    Added changePlaylist click handler to props in playlists component

--- client/src/components/Playlists.jsx
+++ client/src/components/Playlists.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-const Playlists = ({ playlists }) => {
-
-  return (
+const Playlists = ({ playlists, changePlaylist }) => {
+return playlists ?
+  (
     <div>
       <br/>
       <h3>Your Playlists: </h3>
       {playlists.map((pl, i) => {
-        return <div className='playlist' key={i}>{pl}</div>
+        return <div className='playlist' key={i} onClick={changePlaylist}>{pl}</div>
       })}
 
     </div>
   )
+  : null;
 }
 
 
