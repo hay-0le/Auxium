e3d1f6f96438e0ba02b4ca5f6e4be51cb59a1937
hay-0le <haley.olsen1@gmail.com>
2020-02-26 22:11:03 -0800

    Each playlist now comes as an object, with an playlistid and a name. -Updated mapping to set id to the playlistid and the pass name as usualy

--- client/src/components/Playlists.jsx
+++ client/src/components/Playlists.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Playlists = ({ playlists, changePlaylist }) => {
+  console.log("PPPPP", playlists)
 return playlists ?
   (
     <div>
       <br/>
       <h3>Your Playlists: </h3>
       {playlists.map((pl, i) => {
-        return <div className='playlist' key={i} onClick={changePlaylist}>{pl}</div>
+        const { playlistid, playlistname } = pl;
+
+        return <div className='playlist' id={playlistid} onClick={changePlaylist}>{playlistname}</div>
       })}
 
     </div>
